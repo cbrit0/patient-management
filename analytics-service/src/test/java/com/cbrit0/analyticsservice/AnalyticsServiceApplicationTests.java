25a5f3cf10e7f3230a598024a907a345e1c2d2e3
cbrit0 <cristobal.brito.p@gmail.com>
2025-06-29 19:37:01 -0400

    add kafka consumer to analytics service

---
+++ analytics-service/src/test/java/com/cbrit0/analyticsservice/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbrit0.analyticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
